7421212f270ba4bc2181c6850a37102530cb5396
punkyman <julien.reiss@gmail.com>
2019-02-09 10:31:07 +0100

    add download and upload tags

--- include/constant_data.h
+++ include/constant_data.h
@@ -2,3 +2,9 @@ const char* WIFI_SSID = "";
 const char* WIFI_KEY = "";
 
 const char* HTTP_BOX_URL = "http://192.168.0.1/html/home.html";
+
+// <label class="list_right" id="TotalDownload">5.41 Go</label>
+const char* HTML_DOWNLOAD_TAG = "TotalDownload";
+
+// <label class="list_right" id="TotalUpload">324.15 Mo</label>
+const char* HTML_UPLOAD_TAG = "TotalUpload";
