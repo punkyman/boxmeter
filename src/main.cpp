eb7b25a9af08368b48526abbcd3d4b89df15baf4
punkyman <julien.reiss@gmail.com>
2019-02-09 08:47:59 +0100

    implement wifi connection, show ip adress on success

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+#define USE_SERIAL Serial
+
+const char* WIFI_SSID = "";
+const char* WIFI_KEY = "";
 
 LiquidCrystal lcd(22,23,5,18,19,21);
 
@@ -7,11 +14,29 @@ void setup()
 {
     lcd.begin(16, 2);
     lcd.clear();
-    lcd.print("How to Interface");
 
-    // go to row 1 column 0, note that this is indexed at 0
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_SSID, WIFI_KEY);
+
+    lcd.setCursor(0,0);
+    lcd.print("  Connecting to ");
     lcd.setCursor(0,1); 
-    lcd.print ("LCD with ESP32");
+    lcd.print("     Wifi...    ");
+
+    while(WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+    }
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("  Success, IP : ");
+    lcd.setCursor(0,1);
+    lcd.print(WiFi.localIP());
+    delay(2000);
+
 }
 
-void loop(){}
+void loop()
+{
+}
