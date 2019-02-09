453644011561a03841fd8b1f858535b3a4af07eb
punkyman <julien.reiss@gmail.com>
2019-02-09 10:12:29 +0100

    implement http data parsing

--- src/main.cpp
+++ src/main.cpp
@@ -39,15 +39,33 @@ void loop()
     HTTPClient http;
 
     http.begin(HTTP_BOX_URL);
-
     int httpCode = http.GET();
-    if(httpCode > 0) {
-        LCDPrint("  HTTP success  ", "");
+
+    if(httpCode > 0)
+    {
+        WiFiClient* stream = http.getStreamPtr();
+        String downloadData("D : ");
+        String uploadData("U : ");
+        char unused[2];
+
+        if(stream->find(HTML_DOWNLOAD_TAG))
+        {
+            stream->readBytes(unused, 2); // reads ">
+            downloadData += stream->readStringUntil('<');
+        }
+
+        if(stream->find(HTML_UPLOAD_TAG))
+        {
+            stream->readBytes(unused, 2); // reads ">
+            uploadData += stream->readStringUntil('<');
+        }
+
+        LCDPrint(downloadData.c_str(), uploadData.c_str());
     }
     else
     {
         LCDPrint("  HTTP error :  ", http.errorToString(httpCode).c_str());
     }
 
-    delay(5000);
+    delay(15000);
 }
