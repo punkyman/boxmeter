6cf4de2aa96673ca9e59fd471f1343b73ca287d2
punkyman <julien.reiss@gmail.com>
2019-02-09 09:15:06 +0100

    implement http request, factorize lcd operations in a function, move constant data to a separate file

---
+++ include/constant_data.h
@@ -0,0 +1,4 @@
+const char* WIFI_SSID = "";
+const char* WIFI_KEY = "";
+
+const char* HTTP_BOX_URL = "http://192.168.0.1/html/home.html";
--- src/main.cpp
+++ src/main.cpp
@@ -3,40 +3,51 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-#define USE_SERIAL Serial
-
-const char* WIFI_SSID = "";
-const char* WIFI_KEY = "";
+#include "constant_data.h"
 
 LiquidCrystal lcd(22,23,5,18,19,21);
+
+void LCDPrint(const char* line1, const char* line2)
+{
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(line1);
+    lcd.setCursor(0,1); 
+    lcd.print(line2);
+}
 
 void setup()
 {
     lcd.begin(16, 2);
-    lcd.clear();
 
     WiFi.mode(WIFI_STA);
     WiFi.begin(WIFI_SSID, WIFI_KEY);
 
-    lcd.setCursor(0,0);
-    lcd.print("  Connecting to ");
-    lcd.setCursor(0,1); 
-    lcd.print("     Wifi...    ");
+    LCDPrint("  Connecting to ","     Wifi...    ");
 
     while(WiFi.status() != WL_CONNECTED)
     {
         delay(500);
     }
 
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("  Success, IP : ");
-    lcd.setCursor(0,1);
-    lcd.print(WiFi.localIP());
+    LCDPrint("  Success, IP : ", WiFi.localIP().toString().c_str());
     delay(2000);
-
 }
 
 void loop()
 {
+    HTTPClient http;
+
+    http.begin(HTTP_BOX_URL);
+
+    int httpCode = http.GET();
+    if(httpCode > 0) {
+        LCDPrint("  HTTP success  ", "");
+    }
+    else
+    {
+        LCDPrint("  HTTP error :  ", http.errorToString(httpCode).c_str());
+    }
+
+    delay(5000);
 }
