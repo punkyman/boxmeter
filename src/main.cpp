fa05f0134159478c68bc3d3e5af254d6bdca80b4
punkyman <julien.reiss@gmail.com>
2019-02-11 13:08:08 +0100

    rewrite main loop to handle token validity

--- src/main.cpp
+++ src/main.cpp
@@ -34,78 +34,78 @@ void setup()
 
     LCDPrint("  Success, IP : ", WiFi.localIP().toString().c_str());
     delay(2000);
-
-    LCDPrint("    Fetching    ", "    token...    ");
-    
-    HTTPClient http;
-    http.begin(API_TOKEN_URL);
-    int httpCode = http.GET();
-
-    if(httpCode > 0)
-    {
-        WiFiClient* stream = http.getStreamPtr();
-        char unused;
-        if(stream->find(XML_SESSIONINFO_TAG))
-        {
-            stream->readBytes(&unused, 1);
-            token = stream->readStringUntil('<');
-        }
-    }
-
-    LCDPrint("    Fetching    ", "     info...    ");
 }
 
 void loop()
 {
     HTTPClient http;
 
-    http.begin(API_STATISTICS_URL);
-    http.addHeader("Cookie", token.c_str());
+    if(token.length() == 0)
+    {
+        LCDPrint("    Fetching    ", "    token...    ");
+        http.begin(API_TOKEN_URL);
+    }
+    else
+    {
+        http.begin(API_STATISTICS_URL);
+        http.addHeader("Cookie", token.c_str());
+    }
+
     int httpCode = http.GET();
 
     if(httpCode > 0)
     {
-        Serial.write("OK");
-
         WiFiClient* stream = http.getStreamPtr();
-        String longData;
-        long downloadbytes = 0;
-        long uploadbytes = 0;
-        char unused;
 
-        if(stream->find(XML_UPLOAD_TAG))
+        if(token.length() == 0)
         {
-            stream->readBytes(&unused, 1); // reads >
-            longData = stream->readStringUntil('<');
-            uploadbytes = longData.toInt();
+            if(stream->find(XML_SESSIONINFO_TAG))
+            {
+                stream->read(); // removes '>'
+                token = stream->readStringUntil('<');
+            }
         }
+        else
+        {    
+            String longData;
+            long downloadbytes = 0;
+            long uploadbytes = 0;
 
-        if(stream->find(XML_DOWNLOAD_TAG))
-        {
-            stream->readBytes(&unused, 1); // reads >
-            longData = stream->readStringUntil('<');
-            downloadbytes = longData.toInt();
+            if(stream->find(XML_UPLOAD_TAG))
+            {
+                stream->read(); // removes '>'
+                longData = stream->readStringUntil('<');
+                uploadbytes = longData.toInt();
+            }
+
+            if(stream->find(XML_DOWNLOAD_TAG))
+            {
+                stream->read(); // removes '>'
+                longData = stream->readStringUntil('<');
+                downloadbytes = longData.toInt();
+            }
+            
+            char downloadData[17];
+            char uploadData[17];
+
+            if(downloadbytes > GO_AS_BYTES)
+                sprintf(downloadData, "D : %.2f Go", downloadbytes / BYTES_TO_GO);        
+            else
+                sprintf(downloadData, "D : %.2f Mo", downloadbytes / BYTES_TO_MO);
+
+            if(uploadbytes > GO_AS_BYTES)
+                sprintf(uploadData, "U : %.2f Go", uploadbytes / BYTES_TO_GO);
+            else
+                sprintf(uploadData, "U : %.2f Mo", uploadbytes / BYTES_TO_MO);
+
+            LCDPrint(downloadData, uploadData);
         }
-        
-        char downloadData[17];
-        char uploadData[17];
-
-        if(downloadbytes > GO_AS_BYTES)
-            sprintf(downloadData, "D : %.2f Go", downloadbytes / BYTES_TO_GO);        
-        else
-            sprintf(downloadData, "D : %.2f Mo", downloadbytes / BYTES_TO_MO);
-
-        if(uploadbytes > GO_AS_BYTES)
-            sprintf(uploadData, "U : %.2f Go", uploadbytes / BYTES_TO_GO);
-        else
-            sprintf(uploadData, "U : %.2f Mo", uploadbytes / BYTES_TO_MO);
-
-        LCDPrint(downloadData, uploadData);
     }
     else
     {
         LCDPrint("  HTTP error :  ", http.errorToString(httpCode).c_str());
+        token = "";
     }
 
-    delay(5000);
+    delay(10000);
 }
